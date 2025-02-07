fbd10fdb4f3ef1b9cf120ac283748730ab06fbc6
waseemio <bavan98383@gmail.com>
2025-02-07 11:06:08 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://www.facebook.com/` 
+					`https://www.youtube.com/` 
 				}`,
 			},
 		};
