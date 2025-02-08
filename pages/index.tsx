da8fdcc52d92230dc4bb34c590263f439cd145e9
waseemio <bavan98383@gmail.com>
2025-02-08 14:04:30 +0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,15 +6,19 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <Image
-        src="/2.jpg"  // Image ka path jo aapne public folder mein daala tha
-        alt="My Image Asif Ali"
-        width={1200}  // Aap apne image ke size ke according adjust kar sakte hain
-        height={630} // Aap apne image ke size ke according adjust kar sakte hain
-      />
+      {/* Image ko ek <a> tag ke andar wrap kiya gaya hai */}
+      <a href="https://www.yourwebsite.com" target="_blank" rel="noopener noreferrer">
+        <Image
+          src="/2.jpg"  // Image ka path jo aapne public folder mein daala hai
+          alt="My Image Asif Ali"
+          width={1200}  
+          height={630} 
+        />
+      </a>
     </div>
   )
 }
 
 export default Home
+
 
