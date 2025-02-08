b4b2bcc46bdf803628354f7de13519c22e168388
waseemio <bavan98383@gmail.com>
2025-02-08 13:48:59 +0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,10 @@ const Home: NextPage = () => {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       <Image
-        src="/1.jpg"  // Image ka path jo aapne public folder mein daala tha
+        src="/2.jpg"  // Image ka path jo aapne public folder mein daala tha
         alt="My Image Asif Ali"
-        width={1500}  // Aap apne image ke size ke according adjust kar sakte hain
-        height={1000} // Aap apne image ke size ke according adjust kar sakte hain
+        width={1200}  // Aap apne image ke size ke according adjust kar sakte hain
+        height={630} // Aap apne image ke size ke according adjust kar sakte hain
       />
     </div>
   )
