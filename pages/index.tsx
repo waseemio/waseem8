7ebeff79a1aeb043c72a7f63901139254bfab4e3
waseemio <bavan98383@gmail.com>
2025-02-08 14:10:46 +0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,23 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      {/* Image ko ek <a> tag ke andar wrap kiya gaya hai */}
-      <a href="facebook.com" target="youtube.com" rel="noopener noreferrer">
+      {/* Image wrapped inside a link */}
+      <a href="https://deceivedaisle.com/b2n0cj0ppm?key=ff762981bb659c924c5d768535acfc66" target="_blank" rel="noopener noreferrer">
         <Image
-          src="/2.jpg"  // Image ka path jo aapne public folder mein daala hai
+          src="/2.jpg"  // Image ka path jo aapne public folder mein daala tha
           alt="My Image Asif Ali"
           width={1200}  
-          height={630} 
+          height={630}  
+          style={{ cursor: 'pointer' }}
         />
       </a>
     </div>
-  )
+  );
 }
 
-export default Home
-
-
+export default Home;
