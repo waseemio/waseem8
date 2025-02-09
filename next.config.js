b0b8cd45a7d2559526a68fae0ee36dae1ba3313c
waseemio <bavan98383@gmail.com>
2025-02-09 21:09:40 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: '/old-page',
-        destination: '/new-page',
-        permanent: true,
+        source: '/how-to-earn-real-money-online-in-2025',
+        destination: 'http://ahmed4.free.nf/how-to-earn-real-money-online-in-2025',
+        permanent: false,
       },
     ];
   },
